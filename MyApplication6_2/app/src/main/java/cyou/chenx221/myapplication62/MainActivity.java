b7bbe930a37a75cfaa6d6d0daa7fc27e83a4446e
Chenx221 <chenx221@yandex.com>
2021-12-23 21:11:36 +0800

    up

---
+++ MyApplication6_2/app/src/main/java/cyou/chenx221/myapplication62/MainActivity.java
@@ -0,0 +1,15 @@
+package cyou.chenx221.myapplication62;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main62_ui_layout);
+    }
+}
---
+++ TestApplication/app/src/main/java/cyou/chenx221/testapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package cyou.chenx221.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xxvideo_layout);
+    }
+}
