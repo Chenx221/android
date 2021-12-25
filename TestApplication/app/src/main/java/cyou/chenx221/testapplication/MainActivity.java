c30feb791e7d817b9cc2fbc381ea4239bfdeceee
Chenx221 <chenx221@yandex.com>
2021-12-25 17:02:59 +0800

    update

--- TestApplication/app/src/main/java/cyou/chenx221/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/cyou/chenx221/testapplication/MainActivity.java
@@ -14,22 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.xxvideo_layout);
-        setContentView(R.layout.color_select_linearlayout);
+//        setContentView(R.layout.color_select_linearlayout);
+        setContentView(R.layout.register_linearlayout);
 
     }
 
-    public void rb1_onclick(View view) {
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.show_color_pad);
-        linearLayout.setBackgroundColor(Color.GRAY);
-    }
-
-    public void rb2_onclick(View view) {
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.show_color_pad);
-        linearLayout.setBackgroundColor(getResources().getColor(R.color.pink));
-    }
-
-    public void rb3_onclick(View view) {
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.show_color_pad);
-        linearLayout.setBackgroundColor(Color.YELLOW);
-    }
 }
